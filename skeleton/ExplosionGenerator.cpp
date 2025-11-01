b833c88c5b415cf5a61409f5973531bb9f34368a
David Benito Aller <157039413+Reycka@users.noreply.github.com>
2025-11-01 21:03:33 +0100

    Apartado Opcional de la Explosion

--- skeleton/ExplosionGenerator.cpp
+++ skeleton/ExplosionGenerator.cpp
@@ -1,12 +1,13 @@
 #include "ExplosionGenerator.h"
 #include <cmath>
-ExplosionGenerator::ExplosionGenerator(Vector3 _pos, float _R, float _cteR, float _K)
+ExplosionGenerator::ExplosionGenerator(Vector3 _pos, float _R, float _cteR, float _K, Vector3 _velExp)
 {
     pos = _pos;
     R = _R;
     cteR = _cteR;
     K = _K;
     r = 0;
+    velExp = _velExp;
     isActive = false;
 }
 
@@ -19,13 +20,18 @@ Vector3 ExplosionGenerator::addForce(Vector3 entPos, Vector3 entVel, float t)
     explosionForce = Vector3({ 0.0,0.0,0.0 });
     if (isActive) {
         r = pow((entPos.x - pos.x), 2) + pow((entPos.y - pos.y), 2) + pow((entPos.z - pos.z), 2);
+        float dotr = r;
+        r = std::sqrt(r);
+        float auxR = R;
+        R = velExp.magnitude() * t;
         if (isOnArea(entPos)) {
-            float aux1 = K / r; //Saca el valor de cte K / r
+            float aux1 = K / dotr; //Saca el valor de cte K / r
             float aux2 = pow(std::exp(1.0f), -(t / cteR)); //Saca el valor de e elevado a -t/cteR
             explosionForce.x = aux1 * (entPos.x - pos.x) * aux2;
             explosionForce.y = aux1 * (entPos.y - pos.y) * aux2;
             explosionForce.z = aux1 * (entPos.z - pos.z) * aux2;
         }
+        R = auxR;
     }
     return explosionForce;
 }
--- skeleton/ExplosionGenerator.h
+++ skeleton/ExplosionGenerator.h
@@ -6,12 +6,13 @@ private:
 	const float MAXEXPLOSIONTIME = 2.0;
 	Vector3 explosionForce;
 	Vector3 pos;
+	Vector3 velExp;
 	float r;
 	float cteR;
 	float R;
 	float K;
 public:
-	ExplosionGenerator(Vector3 _pos, float _R,float _cteR, float _K);
+	ExplosionGenerator(Vector3 _pos, float _R,float _cteR, float _K,Vector3 _velExp);
 	~ExplosionGenerator();
 	Vector3 addForce(Vector3 entPos,Vector3 entVel,float t = 0) override;
 	bool isOnArea(Vector3 entPos) override;
